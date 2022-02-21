9e29d2965ed2ae64599c6bfa564d0a0b3f531cda
Snowli11 <62450413+Snowli11@users.noreply.github.com>
2022-02-21 22:00:39 +0100

    Added devnet / mainnet channel seperation

--- bot_api/bot.py
+++ bot_api/bot.py
@@ -19,92 +19,168 @@ if os_type == "Linux":
 TOKEN = config['token']
 
 bot = commands.Bot(command_prefix='/')
-old_all_nodes_status = []
-old_list_healthy = []
-old_list_unhealthy = []
-flag = 1
+old_all_nodes_status_mainnet = []
+old_all_nodes_status_devnet = []
+old_list_healthy_mainnet = []
+old_list_healthy_devnet = []
+old_list_unhealthy_mainnet = []
+old_list_unhealthy_devnet = []
+flag_mainnet = 1
+flag_devnet = 1
 
 @bot.event
 async def on_ready():
-    global ctx
-    channel_id = (config['channel_id'])
-    ctx = bot.get_channel(int(channel_id))
-    await ctx.send("Broadcasting pool state")
+    global ctx_mainnet
+    global ctx_devnet
+    channel_id_mainnet = (config['channel_id_mainnet'])
+    channel_id_devnet = (config['channel_id_devnet'])
+    ctx_mainnet = bot.get_channel(int(channel_id_mainnet))
+    ctx_devnet = bot.get_channel(int(channel_id_devnet))
+    await ctx_mainnet.send("Broadcasting pool state")
+    await ctx_devnet.send("Broadcasting pool state")
     update.start()
     
 
 @tasks.loop(seconds=20)
 async def update():
-    global flag
-    global ctx
-    global old_all_nodes_status
-    global old_list_healthy
-    global old_list_unhealthy
-    list_nodes = []
-    list_healthy = []
-    list_unhealthy = []
+    global flag_mainnet
+    global ctx_mainnet
+    global old_all_nodes_status_mainnet
+    global old_list_healthy_mainnet
+    global old_list_unhealthy_mainnet
+    list_nodes_mainnet = []
+    list_healthy_mainnet = []
+    list_unhealthy_mainnet = []
     print("Contacting Api")
     api = config['api']
     raw = requests.get(api) #gets the api request
     temp = raw.json()
     for i in temp:
-        list_nodes.append(i) #loops through the list of nodes and adds them to array
+        list_nodes_mainnet.append(i) #loops through the list of nodes and adds them to array
 
-    all_nodes_status = []
+    all_nodes_status_mainnet = []
     y = 0
     for i in temp: #loops through the list of nodes and adds them to array
-        shrt = temp[f'{list_nodes[y]}']['dlt.green']['isHealthy']
-        all_nodes_status.append(shrt)
+        shrt = temp[f'{list_nodes_mainnet[y]}']['dlt.green']['isMainnetHealthy']
+        all_nodes_status_mainnet.append(shrt)
         y += 1
-    if all_nodes_status != old_all_nodes_status: #
-        print(f"all_nodes_status = {all_nodes_status}")
-        lie = all(all_nodes_status) # checks if there is a False in the array
+    if all_nodes_status_mainnet != old_all_nodes_status_mainnet: #
+        print(f"all_nodes_status = {all_nodes_status_mainnet}")
+        lie = all(all_nodes_status_mainnet) # checks if there is a False in the array
         if lie == False: #if there is a False in the array lie = False
             v = 0
             for i in temp: #loops through the list of nodes and adds them to array
-                if temp[f'{list_nodes[v]}']['dlt.green']['isHealthy'] is True:
-                    shrt_healthy = list_nodes[v]
-                    list_healthy.append(shrt_healthy)
-                if temp[f'{list_nodes[v]}']['dlt.green']['isHealthy'] is False:
-                    shrt_unhealthy = list_nodes[v]
-                    list_unhealthy.append(shrt_unhealthy)
+                if temp[f'{list_nodes_mainnet[v]}']['dlt.green']['isMainnetHealthy'] is True:
+                    shrt_healthy_mainnet = list_nodes_mainnet[v]
+                    list_healthy_mainnet.append(shrt_healthy_mainnet)
+                if temp[f'{list_nodes_mainnet[v]}']['dlt.green']['isMainnetHealthy'] is False:
+                    shrt_unhealthy_mainnet = list_nodes_mainnet[v]
+                    list_unhealthy_mainnet.append(shrt_unhealthy_mainnet)
                 v += 1
             
-            difference_healthy = [x for x in list_healthy if x not in old_list_healthy]
-            difference_unhealthy = [x for x in list_unhealthy if x not in old_list_unhealthy]
-            print(difference_healthy)
-            print(difference_unhealthy)
+            difference_healthy_mainnet = [x for x in list_healthy_mainnet if x not in old_list_healthy_mainnet]
+            difference_unhealthy_mainnet = [x for x in list_unhealthy_mainnet if x not in old_list_unhealthy_mainnet]
+            print(difference_healthy_mainnet)
+            print(difference_unhealthy_mainnet)
             
-            if flag == 0:
+            if flag_mainnet == 0:
                 x = 0
-                for i in difference_unhealthy: #send all unhealthy nodes
-                    await ctx.send(f"```diff\n- {difference_unhealthy[x]} is now unhealthy\n```")
+                for i in difference_unhealthy_mainnet: #send all unhealthy nodes
+                    await ctx_mainnet.send(f"```diff\n- {difference_unhealthy_mainnet[x]} is now unhealthy\n```")
                     x +=1
                     time.sleep(0.5)
 
                 x = 0  
-                for i in difference_healthy: #send all healthy nodes
-                    await ctx.send(f"```yaml\n+ {difference_healthy[x]} is now healthy\n```")
+                for i in difference_healthy_mainnet: #send all healthy nodes
+                    await ctx_mainnet.send(f"```yaml\n+ {difference_healthy_mainnet[x]} is now healthy\n```")
                     x +=1
                     time.sleep(0.5)
                 x = 0
-                old_list_healthy = list_healthy
-                old_list_unhealthy = list_unhealthy
+                old_list_healthy_mainnet = list_healthy_mainnet
+                old_list_unhealthy_mainnet = list_unhealthy_mainnet
             else:
-                old_list_healthy = list_healthy
-                old_list_unhealthy = list_unhealthy
+                old_list_healthy_mainnet = list_healthy_mainnet
+                old_list_unhealthy_mainnet = list_unhealthy_mainnet
                 
         else:
-            result = "```yaml\nAll nodes are healthy\n```"
-            await ctx.send(result)
+            await ctx_mainnet.send("```yaml\nAll nodes are healthy\n```")
+    
       
-    old_all_nodes_status = all_nodes_status
+    old_all_nodes_status_mainnet = all_nodes_status_mainnet
     
-    print(flag)
-    print(list_healthy)
-    print(list_unhealthy)
-    print(list(old_list_healthy))
-    print(list(old_list_unhealthy))
-    flag = 0
+    print(flag_mainnet)
+    print(list_healthy_mainnet)
+    print(list_unhealthy_mainnet)
+    print(list(old_list_healthy_mainnet))
+    print(list(old_list_unhealthy_mainnet))
+    flag_mainnet = 0
+
+    global flag_devnet
+    global ctx_devnet
+    global old_all_nodes_status_devnet
+    global old_list_healthy_devnet
+    global old_list_unhealthy_devnet
+    list_nodes_devnet = []
+    list_healthy_devnet = []
+    list_unhealthy_devnet = []
+    for i in temp:
+        list_nodes_devnet.append(i) #loops through the list of nodes and adds them to array
+
+    all_nodes_status_devnet = []
+    y = 0
+    for i in temp: #loops through the list of nodes and adds them to array
+        shrt = temp[f'{list_nodes_devnet[y]}']['dlt.green']['isDevnetHealthy']
+        all_nodes_status_devnet.append(shrt)
+        y += 1
+    if all_nodes_status_devnet != old_all_nodes_status_devnet: #
+        print(f"all_nodes_status = {all_nodes_status_devnet}")
+        lie = all(all_nodes_status_devnet) # checks if there is a False in the array
+        if lie == False: #if there is a False in the array lie = False
+            v = 0
+            for i in temp: #loops through the list of nodes and adds them to array
+                if temp[f'{list_nodes_devnet[v]}']['dlt.green']['isDevnetHealthy'] is True:
+                    shrt_healthy_devnet = list_nodes_devnet[v]
+                    list_healthy_devnet.append(shrt_healthy_devnet)
+                if temp[f'{list_nodes_devnet[v]}']['dlt.green']['isDevnetHealthy'] is False:
+                    shrt_unhealthy_devnet = list_nodes_devnet[v]
+                    list_unhealthy_devnet.append(shrt_unhealthy_devnet)
+                v += 1
+            
+            difference_healthy_devnet = [x for x in list_healthy_devnet if x not in old_list_healthy_devnet]
+            difference_unhealthy_devnet = [x for x in list_unhealthy_devnet if x not in old_list_unhealthy_devnet]
+            print(difference_healthy_devnet)
+            print(difference_unhealthy_devnet)
+            
+            if flag_devnet == 0:
+                x = 0
+                for i in difference_unhealthy_devnet: #send all unhealthy nodes
+                    await ctx_devnet.send(f"```diff\n- {difference_unhealthy_devnet[x]} is now unhealthy\n```")
+                    x +=1
+                    time.sleep(0.5)
+
+                x = 0  
+                for i in difference_healthy_devnet: #send all healthy nodes
+                    await ctx_devnet.send(f"```yaml\n+ {difference_healthy_devnet[x]} is now healthy\n```")
+                    x +=1
+                    time.sleep(0.5)
+                x = 0
+                old_list_healthy_devnet = list_healthy_devnet
+                old_list_unhealthy_devnet = list_unhealthy_devnet
+            else:
+                old_list_healthy_devnet = list_healthy_devnet
+                old_list_unhealthy_devnet = list_unhealthy_devnet
+                
+        else:
+            await ctx_devnet.send("```yaml\nAll nodes are healthy\n```")
     
+      
+    old_all_nodes_status_devnet = all_nodes_status_devnet
+    
+    print(flag_devnet)
+    print(list_healthy_devnet)
+    print(list_unhealthy_devnet)
+    print(list(old_list_healthy_devnet))
+    print(list(old_list_unhealthy_devnet))
+    flag_devnet = 0
+
 bot.run(TOKEN)
